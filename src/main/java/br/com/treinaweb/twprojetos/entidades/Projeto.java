570f2c05e701c219bd56c40b2ac64a89de41bb98
Nilson Melo <nilson.melo@sti.ufc.br>
2023-09-15 12:29:02 -0300

    feature: controller cliente

--- src/main/java/br/com/treinaweb/twprojetos/entidades/Projeto.java
+++ src/main/java/br/com/treinaweb/twprojetos/entidades/Projeto.java
@@ -20,8 +20,12 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.format.annotation.NumberFormat.Style;
+import org.springframework.hateoas.server.core.Relation;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
+@Relation(collectionRelation = "projetos")
 public class Projeto extends Entidade {
 
     @NotNull
@@ -43,12 +47,14 @@ public class Projeto extends Entidade {
     @DateTimeFormat(iso = ISO.DATE)
     private LocalDate dataFim;
 
+    @JsonIgnore
     @Valid
     @NotNull
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "cliente_id_fk", nullable = false)
     private Cliente cliente;
 
+    @JsonIgnore
     @Valid
     @NotNull
     @ManyToOne(fetch = FetchType.LAZY)
@@ -65,6 +71,7 @@ public class Projeto extends Entidade {
     @NumberFormat(style = Style.CURRENCY, pattern = "#,##0.00")
     private BigDecimal gastos;
 
+    @JsonIgnore
     @ManyToMany
     @JoinTable(
         name = "projeto_funcionario",
