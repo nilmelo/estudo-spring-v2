cb78c83bc48ac5635029f38c0aa8a4fa60cc25a1
Nilson Melo <nilson.melo@sti.ufc.br>
2023-09-15 16:42:54 -0300

    feature: Projeto Controller

--- src/main/java/br/com/treinaweb/twprojetos/api/hateoas/ProjetoAssembler.java
+++ src/main/java/br/com/treinaweb/twprojetos/api/hateoas/ProjetoAssembler.java
@@ -1,22 +1,66 @@
 package br.com.treinaweb.twprojetos.api.hateoas;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.SimpleRepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
+import br.com.treinaweb.twprojetos.api.controller.ClienteControllerAPI;
+import br.com.treinaweb.twprojetos.api.controller.FuncionarioControllerAPI;
+import br.com.treinaweb.twprojetos.api.controller.ProjetoControllerAPI;
 import br.com.treinaweb.twprojetos.entidades.Projeto;
+import br.com.treinaweb.twprojetos.excecoes.ProjetoNaoEncontradoException;
 
 @Component
 public class ProjetoAssembler implements SimpleRepresentationModelAssembler<Projeto> {
 
     @Override
     public void addLinks(EntityModel<Projeto> resource) {
-        // TODO Auto-generated method stub
+        Long clienteId = resource.getContent().getCliente().getId();
+        Long liderId = resource.getContent().getLider().getId();
+        Long id = resource.getContent().getId();
+
+        Link liderLink = linkTo(methodOn(FuncionarioControllerAPI.class).buscarPorId(liderId))
+            .withRel("lider")
+            .withType("GET");
+
+        Link clienteLink = linkTo(methodOn(ClienteControllerAPI.class).buscarPorId(clienteId))
+            .withRel("cliente")
+            .withType("GET");
+
+        Link selfLink = linkTo(methodOn(ProjetoControllerAPI.class).buscarPorId(id))
+            .withSelfRel()
+            .withType("GET");
+
+        Link editarLink = linkTo(methodOn(ProjetoControllerAPI.class).atualizar(null, id))
+            .withSelfRel()
+            .withType("PUT");
+
+        Link excluirLink = linkTo(methodOn(ProjetoControllerAPI.class).excluirPorId(id))
+            .withSelfRel()
+            .withType("DELETE");
+
+        Link equipeLink = linkTo(methodOn(ProjetoControllerAPI.class).buscarEquipe(id))
+            .withRel("equipe")
+            .withType("GET");
+
+        Link atualizarEquipeLink = linkTo(methodOn(ProjetoControllerAPI.class).atualizarEquipe(id, null))
+            .withRel("equipe")
+            .withType("PATCH");
+
+        resource.add(liderLink, clienteLink, selfLink, editarLink, excluirLink, equipeLink, atualizarEquipeLink);
     }
 
     @Override
     public void addLinks(CollectionModel<EntityModel<Projeto>> resources) {
-        // TODO Auto-generated method stub
+        Link selfLink = linkTo(methodOn(ProjetoControllerAPI.class).buscarTodos(null))
+            .withSelfRel()
+            .withType("GET");
+
+        resources.add(selfLink);
     }
 }
--- src/main/java/br/com/treinaweb/twprojetos/servicos/ProjetoServico.java
+++ src/main/java/br/com/treinaweb/twprojetos/servicos/ProjetoServico.java
@@ -1,10 +1,17 @@
 package br.com.treinaweb.twprojetos.servicos;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import br.com.treinaweb.twprojetos.api.dto.EquipeDTO;
+import br.com.treinaweb.twprojetos.api.dto.ProjetoDTO;
+import br.com.treinaweb.twprojetos.api.mapper.ProjetoMapper;
+import br.com.treinaweb.twprojetos.entidades.Funcionario;
 import br.com.treinaweb.twprojetos.entidades.Projeto;
 import br.com.treinaweb.twprojetos.excecoes.ProjetoNaoEncontradoException;
 import br.com.treinaweb.twprojetos.repositorios.ProjetoRepositorio;
@@ -15,8 +22,18 @@ public class ProjetoServico {
     @Autowired
     private ProjetoRepositorio projetoRepositorio;
 
+    @Autowired
+    private FuncionarioServico funcionarioServico;
+
+    @Autowired
+    private ProjetoMapper projetoMapper;
+
     public List<Projeto> buscarTodos() {
         return projetoRepositorio.findAll();
+    }
+
+    public Page<Projeto> buscarTodos(Pageable paginacao) {
+        return projetoRepositorio.findAll(paginacao);
     }
 
     public Projeto buscarPorId(Long id) {
@@ -28,10 +45,39 @@ public class ProjetoServico {
         return projetoRepositorio.save(projeto);
     }
 
+    public Projeto cadastrar(ProjetoDTO projeto) {
+        return projetoRepositorio.save(projetoMapper.converterParaEntidade(projeto));
+    }
+
     public Projeto atualizar(Projeto projeto, Long id) {
         buscarPorId(id);
 
         return projetoRepositorio.save(projeto);
+    }
+
+    public Projeto atualizar(ProjetoDTO projetoDTO, Long id) {
+        buscarPorId(id);
+        Projeto projeto = projetoMapper.converterParaEntidade(projetoDTO);
+        projeto.setId(id);
+
+        return projetoRepositorio.save(projeto);
+    }
+
+    public List<Funcionario> atualizarEquipe(EquipeDTO equipeDTO, Long id) {
+        Projeto projeto = buscarPorId(id);
+
+        List<Funcionario> equipe = new ArrayList<>();
+
+        equipeDTO.getEquipe().forEach(funcionarioId -> {
+            Funcionario funcionario = funcionarioServico.buscarPorId(funcionarioId);
+
+            equipe.add(funcionario);
+        });
+
+        projeto.setEquipe(equipe);
+        projetoRepositorio.save(projeto);
+
+        return equipe;
     }
 
     public void excluirPorId(Long id) {
